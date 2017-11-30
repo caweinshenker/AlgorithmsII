72d10ff76fe6717fbb70c751dc7986b243dc08ca
Colin Weinshenker <caweinshenker@email.wm.edu>
2017-11-29 22:30:08 -0800

    Transposition optimization broken :(

--- p2/SeamCarver.java
+++ p2/SeamCarver.java
@@ -3,41 +3,58 @@ import java.lang.*;
 import java.awt.Color;
 import edu.princeton.cs.algs4.*;
 
-
-
 public class SeamCarver {
 
-	private Picture picture;
-	private final int HORIZONTAL_SEAM = 0;
-	private final int VERTICAL_SEAM = 1;
 	private double[][] energies;
+	private Color[][] colors;
+	private int width;
+	private int height;
+	private enum ORIENTATION { HORIZONTAL, VERTICAL};
+	private ORIENTATION orientation;
 
 
 	public SeamCarver(Picture picture){
 		if (picture == null)
 			throw new java.lang.IllegalArgumentException("Null picture");
 
-		this.picture = picture;
+		width = picture.width();
+		height = picture.height();
+		colors = new Color[height()][width()];
+		orientation = ORIENTATION.VERTICAL;
 
+
+		for (int row=0; row < height(); row++) {
+			for (int col=0; col < width(); col++) {
+				colors[row][col] = picture.get(col, row);
+			}
+		}
 	}
 
 	public Picture picture() {
-		return new Picture(picture);
+		if (orientation == ORIENTATION.HORIZONTAL)
+			transpose();
+
+		Picture picture = new Picture(width, height);
+		for (int row=0; row < height(); row++) {
+			for (int col=0; col < width(); col++) {
+				picture.set(col, row, colors[row][col]);
+			}
+		}
+		return picture;
 	}
 
 	public int width() {
-		return picture.width();
+		return width;
 	}
 
 
 	public int height()  {
-		return picture.height();
+		return height;
 	}
 
 	public double energy(int col,  int row)  {
-		if (col < 0 || col >= width() || row < 0 || row >= height()){
+		if (col < 0 || col >= width() || row < 0 || row >= height())
 			throw new java.lang.IllegalArgumentException("Coordinates out of bounds");
-		}
 
 		if (col == 0 || col == width() - 1 || row == 0 || row == height() - 1)
 			return 1000;
@@ -46,13 +63,13 @@ public class SeamCarver {
 	}
 
 	private double gradient(int col, int row) {
-		Color left = picture.get(col - 1, row);
-		Color right = picture.get(col + 1, row);
-		Color top = picture.get(col, row - 1);
-		Color bottom = picture.get(col, row + 1);
+		Color left = colors[row][col - 1];
+		Color right = colors[row][col + 1];
+		Color top = colors[row - 1][col];
+		Color bottom = colors[row + 1][col];
 
 		return Math.pow((centralDifferences(left, right) +
-		 		centralDifferences(top, bottom)), 0.5);
+		 	   			 centralDifferences(top, bottom)), 0.5);
 	}
 
 	private double centralDifferences(Color c1, Color c2) {
@@ -61,23 +78,35 @@ public class SeamCarver {
 			   Math.pow(c1.getGreen() - c2.getGreen(), 2);
 	}
 
-	public int[] findHorizontalSeam()   {
-		Picture tmpPicture = picture;
-		picture = transpose(picture);
+	public int[] findHorizontalSeam() {
+		if (orientation != ORIENTATION.HORIZONTAL){
+			transpose();
+			orientation = ORIENTATION.HORIZONTAL;
+		}
+		else orientation = ORIENTATION.VERTICAL;
+
 		int[] seam = findVerticalSeam();
-		picture = transpose(picture);
+
+
 		return seam;
 	}
 
 	public int[] findVerticalSeam()  {
-		Point2D edgeTo[][] = new Point2D[height()][width()];
-		double distTo[][] = new double[height()][width()];
+		Point2D edgeTo[][];
+		double distTo[][];
 
-		//StdOut.printf("Height: %d, Width: %d\n", height(), width());
+		if (orientation == ORIENTATION.HORIZONTAL) {
+			transpose();
+			orientation = ORIENTATION.VERTICAL;
+		}
+
+		edgeTo = new Point2D[height][width];
+		distTo = new double[height][width];
+
 		fillEnergies();
 		initDistToAndEdgeTo(edgeTo, distTo);
-		for (int row=0; row < height(); row++){
-			for (int col=0; col < width(); col++) {
+		for (int row=0; row < height; row++){
+			for (int col=0; col < width; col++) {
 				relaxVertex(row, col, edgeTo, distTo);
 			}
 		}
@@ -102,7 +131,6 @@ public class SeamCarver {
 			curCol = (int) edgeTo[row][curCol].y();
 		}
 		seam[0] = curCol;
-
 
 		return seam;
 	}
@@ -146,19 +174,33 @@ public class SeamCarver {
 	}
 
 	public void removeHorizontalSeam(int[] seam) {
-		validateSeam(seam, HORIZONTAL_SEAM);
-		if (seam == null || width() <= 1 )
-			throw new java.lang.IllegalArgumentException("Cannot remove seam");
+		if (width() <= 1) throw new java.lang.IllegalArgumentException("Cannot remove seam");
+		validateSeam(seam, ORIENTATION.HORIZONTAL);
+		Color[][] newColors = new Color[--height][width];
+		for (int row=0; row < height(); row++) {
+			for (int col=0; col < width(); col++) {
+				if (seam[col] != row)
+					newColors[row][col] = colors[row][col];
+			}
+		}
+		colors = newColors;
 	}
 
 	public void removeVerticalSeam(int[] seam) {
-		validateSeam(seam, HORIZONTAL_SEAM);
-		if (seam == null || height() <= 1)
-			throw new java.lang.IllegalArgumentException("Cannot remove seam");
+		if (height() <= 1) throw new java.lang.IllegalArgumentException("Cannot remove seam");
+		validateSeam(seam, ORIENTATION.HORIZONTAL);
+		Color[][] newColors = new Color[height][--width];
+		for (int row=0; row < height(); row++) {
+			for (int col=0; col < width(); col++) {
+				if (seam[row] != col)
+					newColors[row][col] = colors[row][col];
+			}
+		}
+		colors = newColors;
 	}
 
-	private void validateSeam(int[] seam, int orientation){
-		int expectedSeamLength = orientation == HORIZONTAL_SEAM ? width() : height();
+	private void validateSeam(int[] seam, ORIENTATION orientation){
+		int expectedSeamLength = orientation == ORIENTATION.HORIZONTAL ? width() : height();
 
 		if (seam == null || seam.length != expectedSeamLength)
 			throw new java.lang.IllegalArgumentException("Invalid seam: wrong length");
@@ -171,7 +213,6 @@ public class SeamCarver {
 
 	private void fillEnergies() {
 		energies = new double[height()][width()];
-
 		for (int row=0; row < height(); row++){
 			for (int col=0; col < width(); col++) {
 				energies[row][col] = energy(col, row);
@@ -179,14 +220,19 @@ public class SeamCarver {
 		}
 	}
 
-	private Picture transpose(Picture picture) {
-		Picture newPicture = new Picture(height(), width());
-		for (int col=0; col < width(); col++) {
-			for (int row=0; row < height(); row++){
-				newPicture.set(row, col, picture.get(col, row));
+	private void transpose() {
+		Color[][] transposed = new Color[width()][height()];
+		for (int row=0; row < height(); row++) {
+			for (int col=0; col < width(); col++){
+				transposed[col][row] = colors[row][col];
 			}
 		}
-		return newPicture;
+
+		colors = transposed;
+
+		int tmp = height;
+		height = width;
+		width = tmp;
 	}
 
 }
